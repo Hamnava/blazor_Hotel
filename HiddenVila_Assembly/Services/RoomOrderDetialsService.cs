cdbda26262738356be9ba73e32f4aaab99017985
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-01-04 21:47:20 -0800

    the implementation of room order details is finished

---
+++ HiddenVila_Assembly/Services/RoomOrderDetialsService.cs
@@ -0,0 +1,25 @@
+﻿using HiddenVila_Assembly.Services.IServices;
+using Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HiddenVila_Assembly.Services
+{
+    public class RoomOrderDetialsService : IRoomOrderDetialsService
+    {
+        private readonly HttpClient _client;
+        public RoomOrderDetialsService(HttpClient client)
+        {
+            _client = client;
+        }
+        public Task<RoomOrderDetailsDTO> MarkIsOrderSuccessful(RoomOrderDetailsDTO details)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<RoomOrderDetailsDTO> SaveRooomOrderDetails(RoomOrderDetailsDTO details)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
