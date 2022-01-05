a1b0652d14ea56f382bb5aaab84fb661254afeb1
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-01-04 20:02:59 -0800

    all the method of RoomOrderDetails for the repository implemented

--- Business/Repository/RoomOrderDetailsService.cs
+++ Business/Repository/RoomOrderDetailsService.cs
@@ -3,6 +3,7 @@ using Business.Repository.Interfaces;
 using Common;
 using DataAccess.Data;
 using DataAccess.Enttities;
+using Microsoft.EntityFrameworkCore;
 using Models;
 using System;
 using System.Collections.Generic;
@@ -41,19 +42,54 @@ namespace Business.Repository
             }
         }
 
-        public Task<RoomOrderDetailsDTO> GetRoomOrderDetails(int orderRoomId)
+        public async Task<RoomOrderDetailsDTO> GetRoomOrderDetails(int orderRoomId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                RoomOrderDetails orderDetails = await _context.RoomOrderDetails
+                    .Include(x => x.HotelRoom).ThenInclude(x => x.HotelImages)
+                    .FirstOrDefaultAsync(x => x.Id == orderRoomId);
+                RoomOrderDetailsDTO roomOrderDetailsDTO = _mapper.Map<RoomOrderDetails, RoomOrderDetailsDTO>(orderDetails);
+                roomOrderDetailsDTO.HotelRoomDTO.TotalDays = roomOrderDetailsDTO.CheckOutDate.Subtract(roomOrderDetailsDTO.CheckInDate).Days;
+                return roomOrderDetailsDTO;
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
         }
 
-        public Task<IEnumerable<RoomOrderDetailsDTO>> GetRoomOrderDetails()
+        public async Task<IEnumerable<RoomOrderDetailsDTO>> GetRoomOrderDetails()
         {
-            throw new NotImplementedException();
+            try
+            {
+                IEnumerable<RoomOrderDetailsDTO> roomOrders = _mapper.Map<IEnumerable<RoomOrderDetails>, IEnumerable<RoomOrderDetailsDTO>>
+               (_context.RoomOrderDetails.Include(x => x.HotelRoom));
+
+                return roomOrders;
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+           
         }
 
-        public Task<bool> IsRoomBooked(int roomId, DateTime checkIndate, DateTime checkOutdate)
+        public async Task<bool> IsRoomBooked(int roomId, DateTime checkIndate, DateTime checkOutdate)
         {
-            throw new NotImplementedException();
+            var status = false;
+            var existingRoom =await _context.RoomOrderDetails.Where(x => x.RoomId == roomId && x.IsSuccessFulPayment == true &&
+            // check if checkin date that user wants does not fall between any dates for room that is booked
+            checkIndate.Date > x.CheckInDate && checkIndate.Date < checkOutdate ||
+            // check if chekcout date that user wants does not fall between any dates for room that is booked
+            checkOutdate.Date > x.CheckInDate && checkIndate.Date < x.CheckOutDate
+            ).FirstOrDefaultAsync();
+
+            if (existingRoom != null) status = true;
+
+            return status;
         }
 
         public Task<RoomOrderDetailsDTO> MarkPaymentIsSuccessful(int Id)
